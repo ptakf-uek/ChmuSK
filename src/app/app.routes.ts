f12e226958250f38dbbf7edcc960fc635b022f51
KrystianTaf <krystian.taf@gmail.com>
2024-04-29 20:51:56 +0200

    Implement search bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'search-bar', component: SearchBarComponent}
+];
---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import {MatIcon} from '@angular/material/icon'
+import {MatIconButton} from '@angular/material/button'
+import { filesList } from '../../data/filesList';
+import { FormsModule, NgForm } from '@angular/forms';
+import { NgFor } from "@angular/common";
+
+@Component({
+  selector: 'search-bar',
+  standalone: true,
+  imports: [MatIcon, MatIconButton, FormsModule, NgFor],
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.scss'
+})
+export class SearchBarComponent {
+  searchText: string = '';
+  filteredFiles: any[];
+
+  constructor() {
+    this.filteredFiles = filesList;
+  }
+
+  filterFiles() {
+    this.filteredFiles = filesList.filter(file =>
+      file.name.toLowerCase().includes(this.searchText.toLowerCase())
+    );
+  }
+}
