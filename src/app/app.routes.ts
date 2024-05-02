ff3a7ba16d938272c7293e67b1e18046206b386b
Maciej Jastrzębski <macio.evo@gmail.com>
2024-05-02 21:20:15 +0200

    sign in form added to main page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './login/signIn/signIn.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'signIn', component: SignInComponent}
+];
---
+++ src/app/login/signIn/signIn.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'signIn',
+  standalone: true,
+  imports: [],
+  templateUrl: './signIn.component.html',
+  styleUrl: './signIn.component.scss'
+})
+export class SignInComponent {
+  
+}
